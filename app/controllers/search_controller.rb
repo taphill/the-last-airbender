b781c453288f5d45852afe1da345912a6a4ad2b7
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-11 11:15:50 -0600

    Create serach controller index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,13 @@
 class SearchController < ApplicationController
   def index
+    nation = params[:nation].sub(/_/, ' ')
+
+    conn = Faraday.new('https://last-airbender-api.herokuapp.com/api/')
+
+    response = conn.get("v1/characters?affiliation=#{nation}")
+
+    # json = JSON.parse(response.body, symbolize_names: true)
+    
+    @nation_members = JSON.parse(response.body, symbolize_names: true) 
   end
 end
