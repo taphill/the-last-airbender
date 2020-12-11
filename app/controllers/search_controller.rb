a63d981b442527fa8822feca602b67e5d5b2e80d
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-11 11:54:50 -0600

    Refactor search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,6 @@ class SearchController < ApplicationController
   def index
     nation = params[:nation].sub(/_/, ' ')
 
-    conn = Faraday.new('https://last-airbender-api.herokuapp.com/api/')
-
-    response = conn.get("v1/characters?affiliation=#{nation}")
-
-    json = JSON.parse(response.body, symbolize_names: true) 
-    
-    @nation_members = json.map do |member_data|
-      NationMember.new(member_data)  
-    end
+    @nation_members = SearchFacade.nation_members(nation)
   end
 end
